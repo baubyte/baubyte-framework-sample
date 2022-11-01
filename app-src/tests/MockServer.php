ef00f8efa34fbf2151fff39c12f09dac30426933
baubyte <paredbaez.martin@gmail.com>
2022-11-01 19:05:57 -0300

    mock server class test

---
+++ app-src/tests/MockServer.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Baubyte\Tests;
+
+use Baubyte\HttpMethod;
+use Baubyte\Server;
+
+class MockServer implements Server
+{
+    public function __construct(
+        public string $uri, 
+        public HttpMethod $method
+        ) {
+        $this->uri = $uri;
+        $this->method = $method;
+    }
+
+    public function requestUri(): string
+    {
+        return $this->uri;
+    }
+
+    public function requestMethod(): HttpMethod
+    {
+        return $this->method;
+    }
+
+    public function postData(): array
+    {
+        return [];
+    }
+    
+    public function queryParams(): array
+    {
+        return [];
+    }
+}
+ 
