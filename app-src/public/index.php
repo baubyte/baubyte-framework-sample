d30dce1a49a65781609501bee1150067fddfbc78
baubyte <paredbaez.martin@gmail.com>
2022-11-08 22:56:41 -0300

    response factory

--- app-src/public/index.php
+++ app-src/public/index.php
@@ -12,14 +12,13 @@ use Baubyte\Server\PhpNativeServer;
 $router = new Router();
 
 $router->get('/test', function (Request $request) {
-    $response = new Response();
-    $response->setHeader("Content-Type", "application/json");
-    $response->setContent(json_encode(["message" => "GET OK"]));
-
-    return $response;
+    return Response::text("GET OK");
 });
 $router->post('/test', function(Request $request){
-    return "POST OK";
+    return Response::text("POST OK");
+});
+$router->get('/redirect', function (Request $request) {
+    return Response::redirect("/test");
 });
 
 $server = new PhpNativeServer();
@@ -30,7 +29,5 @@ try {
     $response = $action($request);
     $server->sendResponse($response);
 } catch (HttpNotFoundException $th) {
-    $response = new Response();
-    $response->setStatus(404);
-    $server->sendResponse($response);
+    $server->sendResponse(Response::text("Not Found")->setStatus(404));
 }
