5e82d95acc09f900fa9278b7aea609673a5dc57b
baubyte <paredbaez.martin@gmail.com>
2023-03-03 15:12:49 -0300

    example

--- app-src/config/providers.php
+++ app-src/config/providers.php
@@ -1,7 +1,13 @@
 <?php
 
 return [
+    /**
+     * Services that will be loaded at boot time
+     */
     'boot' => [
+        /**
+         * Baubyte framework services providers
+         */
         Baubyte\Providers\ServerServiceProvider::class,
         Baubyte\Providers\DatabaseDriverServiceProvider::class,
         Baubyte\Providers\SessionStorageServiceProvider::class,
@@ -9,11 +15,21 @@ return [
         Baubyte\Providers\FileStorageDriverServiceProvider::class,
         Baubyte\Providers\AuthenticatorServiceProvider::class,
         Baubyte\Providers\HasherServiceProvider::class,
+        /**
+         * Package services providers
+         */
     ],
+    /**
+     * Services that will be loaded after the boot time
+     */
     'runtime' => [
         App\Providers\RuleServiceProvider::class,
-        App\Providers\RouteServiceProvider::class
+        App\Providers\RouteServiceProvider::class,
+        App\Providers\AppServiceProvider::class
     ],
+    /**
+     * Services that will be loaded only when the application is running in cli mode
+     */
     'cli' => [
         Baubyte\Providers\DatabaseDriverServiceProvider::class,
     ]
